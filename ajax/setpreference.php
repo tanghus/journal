e3107059c9e97ed4a6ac39e7b692effe14370532
Thomas Tanghus <thomas@tanghus.net>
2012-07-05 15:41:11 +0200

    Refactored preference setting.

--- ajax/setpreference.php
+++ ajax/setpreference.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * ownCloud - Addressbook
+ * ownCloud - Journal
  *
  * @copyright 2012 Thomas Tanghus <thomas@tanghus.net>
  *
@@ -19,18 +19,33 @@
  *
  */
 
+/**
+ * @brief Set user preference.
+ * @param $key
+ * @param $value
+ */
+
 OCP\JSON::checkLoggedIn();
 OCP\JSON::checkAppEnabled('journal');
 OCP\JSON::callCheck();
 
-$id = isset($_POST['id'])?$_POST['id']:null;
-if(is_null($id)) {
-	OCP\JSON::error(array('data'=>array('message' => OC_Journal_App::$l10n->t('ID is not set!'))));
+$key = isset($_POST['key'])?$_POST['key']:null;
+$value = isset($_POST['value'])?$_POST['value']:null;
+if(is_null($key)) {
+	OCP\JSON::error(array('data' => array('message' => OC_Journal_App::$l10n->t('Key is not set for: '.$value))));
+	OCP\Util::writeLog('journal', __FILE__.', Key is not set for: '.$value, OCP\Util::ERROR);
 	exit;
 }
-if(OCP\Config::setUserValue(OCP\USER::getUser(), 'journal', 'default_calendar', $id)) {
-	OC_Calendar_Object::delete($id);
-	OCP\JSON::success(array('data' => array( 'id' => $id )));
+
+if(is_null($value)) {
+	OCP\JSON::error(array('data' => array('message' => OC_Journal_App::$l10n->t('Value is not set for: '.$key))));
+	OCP\Util::writeLog('journal', __FILE__.', Value is not set for: '.$key, OCP\Util::ERROR);
+	exit;
+}
+
+if(OCP\Config::setUserValue(OCP\USER::getUser(), 'journal', $key, $value)) {
+	OCP\JSON::success(array('data' => array('key' => $key, 'value' => $value)));
 } else {
-	OCP\JSON::error(array('data' => array('id' => $id, 'message' => OC_Journal_App::$l10n->t('Could not set default calendar: '.$id))));
+	OCP\JSON::error(array('data' => array('key' => $key, 'value' => $value, 'message' => OC_Journal_App::$l10n->t('Could not set preference: '.$key.':'.$value))));
+	OCP\Util::writeLog('journal', __FILE__.', Could not set preference: '.$key.':'.$value, OCP\Util::ERROR);
 }
